fd418be61eb9d8aa5e2a7643170922e0038390f1
sarorosy <sn11std@gmail.com>
2024-12-15 20:43:22 +0530

    tetst

--- src/components/Header.js
+++ src/components/Header.js
@@ -18,7 +18,7 @@ const Header = () => {
 
   const handleLogout = () => {
     sessionStorage.clear();
-    navigate('/login');
+    window.close();
   };
 
   const toggleDetailsPage = () => {
@@ -124,7 +124,7 @@ const Header = () => {
   useEffect(() => {
     fetchNotificationsCount();
     const interval = setInterval(() => {
-      //fetchNotificationsCount(); // Fetch count every 5 seconds
+      fetchNotificationsCount(); // Fetch count every 5 seconds
     }, 5000);
 
     return () => clearInterval(interval); // Cleanup on component unmount
