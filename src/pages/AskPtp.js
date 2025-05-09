57b466e77e17ef51f8cdf5df01075ee8d4a3632b
saro-dev <codersaro@gmail.com>
2025-05-09 16:27:55 +0530

    new

--- src/pages/AskPtp.js
+++ src/pages/AskPtp.js
@@ -42,6 +42,11 @@ function AskPtp({ scopeDetails, quoteId, after, plans }) {
         setPtpLoading(true);
         e.preventDefault();
 
+        if(ptp != "Yes"){
+            toast.error('Please check PTP checkbox');
+            setPtpLoading(false)
+            return; 
+        }
         if (ptp == 'Yes' && !ptpAmount){
             toast.error("Please Enter amount");
             setPtpLoading(false);
